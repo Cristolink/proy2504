0d213db07c6b85f4a26b3aad0dfbf480e3ac858d
cetecom <cetecom@LC08L5729>
2017-05-02 10:55:29 -0300

    Actualización conexión a bd

--- index.php
+++ index.php
@@ -11,6 +11,20 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+            $miconn=new mysqli("10.20.26.58", "root", "avaras08", "datospersonales");
+            
+            if($miconn->connect_errno){
+                echo "Fallo al conectar a MySQL: (" . $miconn->connect_errno. ") ". $miconn->connect_errno;
+            }
+            echo $miconn->host_info. "\n";
+            
+            /* Consultas de selección que devuelven un conjunto de resultados */
+            if ($resultado = $miconn->query("SELECT * FROM persona"))
+                    echo "La selección devolvio N°".$resultado->num_rows."filas";
+                    /* liberar el conjunto de resultados */
+            $resultado->close();
+        
+        
         echo 'Hola Mundo';
         ?>
     </body>
