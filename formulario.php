e61dc9622a53c1998234dc3929fe1bffbcd595ad
cetecom <cetecom@LC08L5729>
2017-05-02 11:25:31 -0300

    Actualización conexión a bd 2

---
+++ formulario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    
+    <body>
+       
+        <form method="post" action="guardar.php">
+            <div>nombre: <input name="nombre" type="text"></div>
+            <div>apellido: <input name="apellido" type="text"></div>
+            <div>fec nacimiento: <input name="fecnacimiento" type="text"></div>
+            <input type="submit" value="Enviar">
+            
+        </form>
+        
+    </body>
+</html>
